8c410758888460f78cc2107faa77956e9255f702
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2022-08-15 21:41:23 +0300

    Preferences Datastore update

--- app/src/main/java/com/dag/mvvmarchitectureexample/ui/information/InformationActivity.kt
+++ app/src/main/java/com/dag/mvvmarchitectureexample/ui/information/InformationActivity.kt
@@ -1,8 +1,12 @@
 package com.dag.mvvmarchitectureexample.ui.information
 
+import android.os.Bundle
+import android.view.View
 import com.dag.mvvmarchitectureexample.R
 import com.dag.mvvmarchitectureexample.base.BaseActivity
 import com.dag.mvvmarchitectureexample.databinding.ActivityInformationBinding
+import com.dag.mvvmarchitectureexample.datastore.preferences.PreferencesDataStore
+import java.util.prefs.Preferences
 import javax.inject.Inject
 
 class InformationActivity:BaseActivity<InformationVM,ActivityInformationBinding>() {
@@ -12,4 +16,14 @@ class InformationActivity:BaseActivity<InformationVM,ActivityInformationBinding>
 
     @Inject
     lateinit var informationVM: InformationVM
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding?.informationTV?.setOnClickListener(buttonClickListener)
+    }
+
+    private val buttonClickListener = View.OnClickListener {
+        viewModel?.writeToPreferencesDatastore(PreferencesDataStore.INFORMATION_SCREEN,true)
+    }
 }
--- app/src/main/java/com/dag/mvvmarchitectureexample/ui/information/InformationVM.kt
+++ app/src/main/java/com/dag/mvvmarchitectureexample/ui/information/InformationVM.kt
@@ -1,6 +1,15 @@
 package com.dag.mvvmarchitectureexample.ui.information
 
+import androidx.datastore.preferences.core.Preferences
+import androidx.lifecycle.viewModelScope
 import com.dag.mvvmarchitectureexample.base.BaseVM
+import com.dag.mvvmarchitectureexample.datastore.preferences.PreferencesDataStore
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class InformationVM :BaseVM(){
+class InformationVM @Inject constructor(val preferencesDataStore: PreferencesDataStore):BaseVM(){
+
+    fun <T> writeToPreferencesDatastore(key: Preferences.Key<T>, value:T) = viewModelScope.launch{
+        preferencesDataStore.write(key,value)
+    }
 }
